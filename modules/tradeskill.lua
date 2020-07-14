7d599da2a2bbee981ea237bb6badd6a34d0d0483
Vicious-wow <68206014+Vicious-wow@users.noreply.github.com>
2020-07-14 22:43:23 +0200

    Updated TradeskillFrame Tooltip
    Since cooldowns aren't really a thing anymore, I've updated the Tradeskill tooltip to now show learned professions (2 main, 3 secondary) with icon, name, current level and max level. Changed colors of the tooltip to be in class color and white only because I think it looks prettier. Right clicking the tradekills now opens the profession spellbook.

--- modules/tradeskill.lua
+++ modules/tradeskill.lua
@@ -33,12 +33,11 @@ function TradeskillModule:OnEnable()
 
   self.tradeskillFrame:Show()
 
-  local prof1, prof2, _ = GetProfessions()
-  self.prof1 = prof1
-  self.prof2 = prof2
-
-  local firstProfID = 0
-  local secondProfID = 0
+  self.firstProf  = { idx=nil, id=nil, name=nil, defIcon=nil, lvl=nil, maxLvl=nil }
+  self.secondProf = { idx=nil, id=nil, name=nil, defIcon=nil, lvl=nil, maxLvl=nil }
+  self.arch       = { idx=nil, id=nil, name=nil, defIcon=nil, lvl=nil, maxLvl=nil }
+  self.fish       = { idx=nil, id=nil, name=nil, defIcon=nil, lvl=nil, maxLvl=nil }
+  self.cook       = { idx=nil, id=nil, name=nil, defIcon=nil, lvl=nil, maxLvl=nil }
 
   self:CreateFrames()
   self:RegisterFrameEvents()
@@ -54,31 +53,45 @@ end
 
 function TradeskillModule:UpdateProfValues()
   --update profession indexes before anything else or receive a million bugs when (un)learning professions
-  self.prof1, self.prof2, _ = GetProfessions() --this is the most important line in the entire fucking module
+  self.firstProf.idx, self.secondProf.idx, self.arch.idx, self.fish.idx, self.cook.idx = GetProfessions() --this is the most important line in the entire fucking module
 
-  --if prof1 doesn't exist, the player hasn't learned any profession and thus the tradeskillFrame is hidden
-  if not self.prof1 then
+  --if firstProf.idx doesn't exist, the player hasn't learned any profession and thus the tradeskillFrame is hidden
+  if not self.firstProf.idx then
     self.tradeskillFrame:Hide()
     return
   end
 
   --player has at least one profession, setting first one. show tradeskillFrame because it might've been hidden before
   self.tradeskillFrame:Show()
-  local _, _, skill1, cap1, _ = GetProfessionInfo(self.prof1)
-  self.firstProfBar:SetMinMaxValues(1, cap1)
-  self.firstProfBar:SetValue(skill1)
+  self.firstProf.name, self.firstProf.defIcon, self.firstProf.lvl, self.firstProf.maxLvl, _, _, self.firstProf.id, _ = GetProfessionInfo(self.firstProf.idx)
+  self.firstProfBar:SetMinMaxValues(1, self.firstProf.maxLvl)
+  self.firstProfBar:SetValue(self.firstProf.lvl)
 
-  --if prof2 doesn't exist, hide the secondProfFrame 
-  if not self.prof2 then
+  --if secondProf.idx doesn't exist, hide the secondProfFrame 
+  if not self.secondProf.idx then
     self.secondProfFrame:Hide()
     return
   end
 
   --player has two profession, setting second one. show secondProfFrame because it might've been hidden before
   self.secondProfFrame:Show()
-  local _, _, skill2, cap2, _ = GetProfessionInfo(self.prof2)
-  self.secondProfBar:SetMinMaxValues(1, cap2)
-  self.secondProfBar:SetValue(skill2)
+  self.secondProf.name, self.secondProf.defIcon, self.secondProf.lvl, self.secondProf.maxLvl, _, _, self.secondProf.id, _ = GetProfessionInfo(self.secondProf.idx)
+  self.secondProfBar:SetMinMaxValues(1, self.secondProf.maxLvl)
+  self.secondProfBar:SetValue(self.secondProf.lvl)
+
+  --update values for secondary professions if they exist (archaeology / fishing / cooking)
+  --update archaeology
+  if self.arch.idx then
+    self.arch.name, self.arch.defIcon, self.arch.lvl, self.arch.maxLvl, _, _, self.arch.id, _ = GetProfessionInfo(self.arch.idx)
+  end
+  --update fishing
+  if self.fish.idx then
+    self.fish.name, self.fish.defIcon, self.fish.lvl, self.fish.maxLvl, _, _, self.fish.id, _ = GetProfessionInfo(self.fish.idx)
+  end
+  --update cooking
+  if self.cook.idx then
+    self.cook.name, self.cook.defIcon, self.cook.lvl, self.cook.maxLvl, _, _, self.cook.id, _ = GetProfessionInfo(self.cook.idx)
+  end
 end
 
 function TradeskillModule:Refresh()
@@ -93,20 +106,20 @@ function TradeskillModule:Refresh()
   --update before doing anything here mister addon creator. if we have no professions, why the fuck would we refresh anything?
   self:UpdateProfValues()
   --get the hell out of this function if we have no professions
-  if not self.prof1 then return end
+  if not self.firstProf.idx then return end
 
   --prepare tradeskillFrame bar width and profession icon size
   local iconSize = db.text.fontSize + db.general.barPadding
   local totalWidth = 0
 
   --setting width and position for profession 1 frame
-  self:StyleTradeskillFrame('firstProf', self.prof1)
+  self:StyleTradeskillFrame('firstProf')
   totalWidth = totalWidth + self.firstProfFrame:GetWidth()
   self.firstProfFrame:SetPoint('LEFT')
 
   -- setting width and position for profession 2 frame if it exists, otherwise its frame is hidden anyway
-  if self.prof2 then
-    self:StyleTradeskillFrame('secondProf', self.prof2)
+  if self.secondProf.idx then
+    self:StyleTradeskillFrame('secondProf')
     totalWidth = totalWidth + self.secondProfFrame:GetWidth()
     self.secondProfFrame:SetPoint('LEFT', self.firstProfFrame, 'RIGHT', 5, 0)
   end
@@ -122,14 +135,13 @@ function TradeskillModule:Refresh()
   self.tradeskillFrame:SetPoint('LEFT', xb:GetFrame('clockFrame'), relativeAnchorPoint, xOffset, 0)
 end
 
-function TradeskillModule:StyleTradeskillFrame(framePrefix, profIndex)
+function TradeskillModule:StyleTradeskillFrame(prefix)
   local db = xb.db.profile
   local iconSize = db.text.fontSize + db.general.barPadding
-  local name, _, skill, cap, _, spellOffset, skillLine, _ = GetProfessionInfo(profIndex)
-  local icon = xb.constants.mediaPath..'profession\\'..self.profIcons[skillLine]
+  local icon = xb.constants.mediaPath..'profession\\'..self.profIcons[self[prefix].id]
 
   local textHeight = floor((xb:GetHeight() - 4) / 2)
-  if skill == cap then
+  if self[prefix].lvl == self[prefix].maxLvl then
     textHeight = db.text.fontSize
   end
 
@@ -138,34 +150,33 @@ function TradeskillModule:StyleTradeskillFrame(framePrefix, profIndex)
     barHeight = 2
   end
 
-  self[framePrefix..'ID'] = skillLine
+  self[prefix..'Icon']:SetTexture(icon)
+  self[prefix..'Icon']:SetSize(iconSize, iconSize)
+  self[prefix..'Icon']:SetPoint('LEFT')
+  self[prefix..'Icon']:SetVertexColor(db.color.normal.r, db.color.normal.g, db.color.normal.b, db.color.normal.a)
 
-  self[framePrefix..'Icon']:SetTexture(icon)
-  self[framePrefix..'Icon']:SetSize(iconSize, iconSize)
-  self[framePrefix..'Icon']:SetPoint('LEFT')
-  self[framePrefix..'Icon']:SetVertexColor(db.color.normal.r, db.color.normal.g, db.color.normal.b, db.color.normal.a)
+  self[prefix..'Text']:SetFont(xb:GetFont(textHeight))
+  self[prefix..'Text']:SetTextColor(db.color.inactive.r, db.color.inactive.g, db.color.inactive.b, db.color.inactive.a)
+  self[prefix..'Text']:SetText(string.upper(self[prefix].name))
 
-  self[framePrefix..'Text']:SetFont(xb:GetFont(textHeight))
-  self[framePrefix..'Text']:SetTextColor(db.color.inactive.r, db.color.inactive.g, db.color.inactive.b, db.color.inactive.a)
-  self[framePrefix..'Text']:SetText(string.upper(name))
-
-  if skill == cap then
-    self[framePrefix..'Text']:SetPoint('LEFT', self[framePrefix..'Icon'], 'RIGHT', 5, 0)
+  if self[prefix].lvl == self[prefix].maxLvl then
+    self[prefix..'Text']:SetPoint('LEFT', self[prefix..'Icon'], 'RIGHT', 5, 0)
   else
-    self[framePrefix..'Text']:SetPoint('TOPLEFT', self[framePrefix..'Icon'], 'TOPRIGHT', 5, 0)
-    self[framePrefix..'Bar']:SetStatusBarTexture(1, 1, 1)
+    self[prefix..'Text']:SetPoint('TOPLEFT', self[prefix..'Icon'], 'TOPRIGHT', 5, 0)
+    self[prefix..'Bar']:SetStatusBarTexture(1, 1, 1)
     if db.modules.tradeskill.barCC then
-      self[framePrefix..'Bar']:SetStatusBarColor(xb:GetClassColors())
+      self[prefix..'Bar']:SetStatusBarColor(xb:GetClassColors())
     else
-      self[framePrefix..'Bar']:SetStatusBarColor(db.color.normal.r, db.color.normal.g, db.color.normal.b, db.color.normal.a)
+      self[prefix..'Bar']:SetStatusBarColor(db.color.normal.r, db.color.normal.g, db.color.normal.b, db.color.normal.a)
     end
-    self[framePrefix..'Bar']:SetSize(self[framePrefix..'Text']:GetStringWidth(), barHeight)
-    self[framePrefix..'Bar']:SetPoint('BOTTOMLEFT', self[framePrefix..'Icon'], 'BOTTOMRIGHT', 5, 0)
+    self[prefix..'Bar']:SetSize(self[prefix..'Text']:GetStringWidth(), barHeight)
+    self[prefix..'Bar']:SetPoint('BOTTOMLEFT', self[prefix..'Icon'], 'BOTTOMRIGHT', 5, 0)
 
-    self[framePrefix..'BarBg']:SetAllPoints()
-    self[framePrefix..'BarBg']:SetColorTexture(db.color.inactive.r, db.color.inactive.g, db.color.inactive.b, db.color.inactive.a)
+    self[prefix..'BarBg']:SetAllPoints()
+    self[prefix..'BarBg']:SetColorTexture(db.color.inactive.r, db.color.inactive.g, db.color.inactive.b, db.color.inactive.a)
   end
-  self[framePrefix..'Frame']:SetSize(iconSize + self[framePrefix..'Text']:GetStringWidth() + 5, xb:GetHeight())
+
+  self[prefix..'Frame']:SetSize(iconSize + self[prefix..'Text']:GetStringWidth() + 5, xb:GetHeight())
 end
 
 function TradeskillModule:CreateFrames()
@@ -182,25 +193,29 @@ function TradeskillModule:CreateFrames()
   self.secondProfBarBg = self.secondProfBarBg or self.secondProfBar:CreateTexture(nil, 'BACKGROUND')
 end
 
-function TradeskillModule:SetProfScripts(framePrefix)
-  self[framePrefix..'Frame']:SetScript('OnMouseDown', function()
-    local _, _, _, _, _, openSkillLine, _ = C_TradeSkillUI.GetTradeSkillLine()
-    if openSkillLine == self[framePrefix..'ID'] then C_TradeSkillUI.CloseTradeSkill() return end
-      C_TradeSkillUI.OpenTradeSkill(self[framePrefix..'ID'])
+function TradeskillModule:SetProfScripts(prefix)
+  self[prefix..'Frame']:SetScript('OnMouseDown', function(_, button)
+    if button == 'LeftButton' then
+      --workaround, toggling spellbooks for tailoring / engineering / inscription is bugged on blizzard's side
+      --i prefer this over the alternative of casting a spell via SecureActionButtonTemplate frames anyway
+      local _, _, _, _, _, openSkillLine, _ = C_TradeSkillUI.GetTradeSkillLine()
+      if openSkillLine == self[prefix].id then C_TradeSkillUI.CloseTradeSkill() return end
+      C_TradeSkillUI.OpenTradeSkill(self[prefix].id)
+    elseif button == 'RightButton' then ToggleSpellBook(BOOKTYPE_PROFESSION) end
   end)
 
-  self[framePrefix..'Frame']:SetScript('OnEnter', function()
+  self[prefix..'Frame']:SetScript('OnEnter', function()
     if InCombatLockdown() then return end
-    self[framePrefix..'Text']:SetTextColor(unpack(xb:HoverColors()))
+    self[prefix..'Text']:SetTextColor(unpack(xb:HoverColors()))
     if xb.db.profile.modules.tradeskill.showTooltip then
       self:ShowTooltip()
     end
   end)
 
-  self[framePrefix..'Frame']:SetScript('OnLeave', function()
+  self[prefix..'Frame']:SetScript('OnLeave', function()
     if InCombatLockdown() then return; end
     local db = xb.db.profile
-    self[framePrefix..'Text']:SetTextColor(db.color.inactive.r, db.color.inactive.g, db.color.inactive.b, db.color.inactive.a)
+    self[prefix..'Text']:SetTextColor(db.color.inactive.r, db.color.inactive.g, db.color.inactive.b, db.color.inactive.a)
     if xb.db.profile.modules.tradeskill.showTooltip then
       GameTooltip:Hide()
     end
@@ -242,18 +257,30 @@ function TradeskillModule:RegisterFrameEvents()
 end
 
 function TradeskillModule:ShowTooltip()
-  --lol wtf is happening here. more "bugfixes"?
-  return
-  --[[
+  local r, g, b, _ = xb:GetClassColors()
   GameTooltip:SetOwner(self.tradeskillFrame, 'ANCHOR_'..xb.miniTextPosition)
-  GameTooltip:AddLine("[|cff6699FF"..L['Cooldowns'].."|r]")
+  GameTooltip:AddLine("|cFFFFFFFF[|r"..TRADE_SKILLS.."|cFFFFFFFF]|r", r, g, b)
   GameTooltip:AddLine(" ")
 
-  local recipeIds = C_TradeSkillUI.GetAllRecipeIDs()
+  if self.firstProf.idx then self:ListTooltipProfession('firstProf', r, g, b) end
+  if self.secondProf.idx then self:ListTooltipProfession('secondProf', r, g, b) end
+  if self.cook.idx then self:ListTooltipProfession('cook', r, g, b) end
+  if self.fish.idx then self:ListTooltipProfession('fish', r, g, b) end
+  if self.arch.idx then self:ListTooltipProfession('arch', r, g, b) end
+
+  --in case there's daily crafts in shadowlands, add a section under the professions for cooldowns
+  --probably works with: C_TradeSkillUI.GetRecipeInfo() and GetSpellCooldown()
 
   GameTooltip:AddLine(" ")
-  GameTooltip:AddDoubleLine('<'..L['Left-Click']..'>', BINDING_NAME_TOGGLECURRENCY, 1, 1, 0, 1, 1, 1)
-  GameTooltip:Show()]]--
+  GameTooltip:AddDoubleLine('<'..L['Left-Click']..'>', L['Toggle Profession Frame'], r, g, b, 1, 1, 1)
+  GameTooltip:AddDoubleLine('<'..L['Right-Click']..'>', L['Toggle Profession Spellbook'], r, g, b, 1, 1, 1)
+  GameTooltip:Show()
+end
+
+function TradeskillModule:ListTooltipProfession(prefix, r, g, b)
+  local left = "|T"..self[prefix].defIcon..":0|t "..self[prefix].name
+  local right = "|cFFFFFFFF"..self[prefix].lvl.."|r / "..self[prefix].maxLvl
+  GameTooltip:AddDoubleLine(left, right, 1, 1, 1, r, g, b)
 end
 
 function TradeskillModule:GetDefaultOptions()
